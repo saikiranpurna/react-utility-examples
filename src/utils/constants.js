e6b19afb5722a091ee29be4d1d6c3b3e763d845c
NarasimhaAnaparthi <narasimharao.anarpathi@engro.io>
2024-07-25 16:05:53 +0530

    added author name to card and css to make back button to float

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,18 +3,21 @@ export const CardItems = [
         title:'React Quill JS [Basic]',
         subTitle:'Quill is a modern rich text editor built for compatibility and extensibility.',
         link:'/quill-basic',
+        author:"Sai Kiran Purna",
         gitLink:'https://github.com/saikiranpurna/react-utility-examples/blob/main/src/examples/QuillJs/TextEditor.jsx'
     },
     {
         title:'React Quill JS [Advanced]',
         subTitle:'Quill is a modern rich text editor built for compatibility and extensibility.',
         link:'/quill-advanced',
+        author:"Sai Kiran Purna",
         gitLink:'https://github.com/saikiranpurna/react-utility-examples/blob/main/src/examples/QuillJs/TextEditor.jsx'
     },  
     {
         title:'Sliders',
-        subTitle:'Various Sliders using only JS & CSS with React',
+        subTitle:'Different types of Sliders/Carousel using DOM manipulation & CSS only with React.',
         link:'/sliders',
+        author:"Narasimha Rao Anaparthi",
         gitLink:'https://github.com/NarasimhaAnaparthi/react-utility-examples/tree/main/src/examples/Sliders'
     },  
 ]
